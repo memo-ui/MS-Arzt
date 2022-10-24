e4d71fcd33b2e47e75653c5600bb4f59ca7278da
memo_ <65392947+memo-ui@users.noreply.github.com>
2022-10-24 14:40:23 +0200

    Update Application.kt

--- main/kotlin/com/acme/arzt/Application.kt
+++ main/kotlin/com/acme/arzt/Application.kt
@@ -1,25 +1,3 @@
-/*
- * Copyright (C) 2016 - present Juergen Zimmermann, Hochschule Karlsruhe
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
-// ACME (= A Company that Makes Everything): fiktiver Unternehmensname in amerikan. Comics
-// haeufig in Beispielen verwendet; aehnlich wie foo und bar
-// griech. Ursprung: Gipfel, Hoehepunkt
-// https://en.wikipedia.org/wiki/Acme_Corporation
-
 package com.acme.arzt
 
 import com.acme.arzt.config.ProfilesBanner.banner
